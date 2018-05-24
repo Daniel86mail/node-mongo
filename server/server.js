d1f6bd82abf0bc4ebe4771af67c1ef03eace6c5b
DanielCiuraru <daniel86mail@gmail.com>
2018-05-24 18:03:39 +0300

    get by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
             todos
         });
     }, (e) => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(ObjectID.isValid(id)){
+        Todo.findById(id).then((todo)=> {
+            if(todo)
+                res.send(todo);
+            else
+                res.status(404).send();
+        }, (e) => res.status(400).send());
+    }else{
+        res.status(404).send();
+    }
 });
 
 app.listen(3000, () => {
