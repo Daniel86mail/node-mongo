07204916b3f8ce8c0eb305c5c422076cc4a6866a
Daniel Ciuraru <daniel86mail@gmail.com>
2018-05-28 17:44:06 +0300

    added test for logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -272,4 +272,24 @@ describe('Server', () => {
             });
         });
     });
+    describe('DELETE /users/me/token', ()=>{
+        it('should delete the token on logout', (done)=>{
+            request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res)=>{
+                if(err)
+                    return done(err);
+
+                User.findById(users[0]._id).then((user)=>{
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e)=>{
+                    done(e);
+                });
+            });
+
+        });
+    });
 });
