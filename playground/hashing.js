ec0e9f75a043776a10bf1ad83ecc38ad254bd864
Daniel Ciuraru <daniel86mail@gmail.com>
2018-05-28 13:47:40 +0300

    added password hashing on user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123456ABC';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash);
+    });
+});
+
+var hasedPassword = '$2a$10$voM/W2R5cxlCmlQ6DiPdSOgBGiRjlFLmdWl9.zLDte9IWQlSzg9Ua';
+
+bcrypt.compare(password, hasedPassword, (err, res) => {
+    console.log(res);
+});
 
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var data = {
+//     id: 10
+// };
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
